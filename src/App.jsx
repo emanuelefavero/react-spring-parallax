4abab82df56a596a6ce43969bf82a3a62aeccde3
emanuelefavero <emafaux89@gmail.com>
2023-02-05 16:07:17 +0100

    add basic setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
+import { useSpring, animated } from '@react-spring/web'
+
 function App() {
+  const [springs, api] = useSpring(() => ({
+    from: { x: 0 },
+  }))
+
+  const handleClick = () => {
+    api.start({
+      from: { x: 0 },
+      to: { x: 100 },
+    })
+  }
+
   return (
     <>
-      <h1>Hello</h1>
+      <animated.div
+        onClick={handleClick}
+        style={{
+          width: 50,
+          height: 50,
+          background: '#c96dff',
+          borderRadius: 16,
+          transition: 'all 0.3s ease',
+          // x: springs.x.to((x) => `${x}%`),
+          ...springs,
+        }}
+      />
     </>
   )
 }
